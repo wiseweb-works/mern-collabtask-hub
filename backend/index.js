90e8907eeb8c75d2d19ddf5515c2a73d6930e865
Wise <wisewebworks@outlook.com>
2025-04-02 12:35:00 +0200

    Initialize Backend structure
    - initial routes, controllers, models, and
    - database connection

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const { connect } = require("http2");
+const connectDB = require("./config/db");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+connectDB();
+
+app.use(express.json());
+
+// app.use('/api/auth', authRoutes);
+// app.use('/api/users', userRoutes);
+// app.use('/api/tasks', taskRoutes);
+// app.use('/api/reports', reportRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
