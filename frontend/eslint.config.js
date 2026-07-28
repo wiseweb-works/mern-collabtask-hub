b3e9416928030ebe915042bbb6d762ea4fa888af
Abdullah Koyuncu <wisewebworks@outlook.com>
2026-07-28 16:36:37 +0200

    fix(security): security vulnerabilities

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,4 +1,3 @@
-import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -7,7 +6,7 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
